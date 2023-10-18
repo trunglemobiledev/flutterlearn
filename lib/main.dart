ad5d29d37a76777d4b5564106c4114f88376cafc
trunglemobiledev <lmt.cn.tv@gmail.com>
2023-10-18 14:32:41 +0700

    Bai 32 StatelessWidget, State, create Widget extends StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,28 @@ void main() {
     home: SafeArea(
         child: Scaffold(
             appBar: AppBar(
-              backgroundColor: Colors.red,
-              title: const Text('Co ban flutter'),
+              backgroundColor: Colors.deepPurpleAccent,
+              title: const Text('Flutter basic'),
             ),
-            body: const Center(child: Text('Hello Trung')))),
+            // body: const Center(child: Text('Hello Trung')))),
+            body: const MyWidget(true))),
     debugShowCheckedModeBanner: false, // tăt cai nhan debug
   ));
+}
+
+// StatelessWidget :
+// Tự tạo MyWidget ke thừa StatelessWidget
+class MyWidget extends StatelessWidget {
+  final bool loading;
+
+  const MyWidget(this.loading, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // hàm buil() bắc buộc phải có trong StatelessWidget
+    // Là 1 hàm càn ghi đè lại
+    return loading
+        ? const Center(child: Text('State'))
+        : const Center(child: CircularProgressIndicator());
+  }
 }
