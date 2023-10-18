1b5f35377ea929e8a365941b51fd9ba6726ed59f
trunglemobiledev <lmt.cn.tv@gmail.com>
2023-10-18 14:17:42 +0700

    Bai 31 Flutter: SafeArea, Scaffold, AppBar, Center, Text

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:flutter/material.dart';
 // B2 Hàm main hàm chạy đầu tiên
 void main() {
   // Hàm runApp(x) là hàm chạy đầu tiên trong main() để run app;
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     // Sử dụng các thành phần giao diện người dùng;
-    home: Text('Hello Trung'),
+    home: SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.red,
+              title: const Text('Co ban flutter'),
+            ),
+            body: const Center(child: Text('Hello Trung')))),
+    debugShowCheckedModeBanner: false, // tăt cai nhan debug
   ));
-
 }
